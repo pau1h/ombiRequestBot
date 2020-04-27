0cdcf638783e72f1a44a611631a6081a3503bbf4
Paul Hawkins <20309256+hawkinspaul@users.noreply.github.com>
2020-04-26 20:42:52 -0400

    added searchMovie.cs

---
+++ requestBot/searchMovie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using TMDbLib.Objects.Movies;
+using TMDbLib.Objects.Search;
+using TMDbLib.Objects.General;
+using TMDbLib.Client;
+//api key b3e5109461a747f4a71d48f3c299b38c
+
+namespace requestBot
+{
+    class searchMovie
+    {
+        public static async Task<SearchContainer<SearchMovie>> getResultsAsync(String mediaName, TMDbClient client)
+        {
+            SearchContainer<SearchMovie> results = await client.SearchMovieAsync(mediaName);
+
+            return results;
+         }
+            
+    }
+}
+
